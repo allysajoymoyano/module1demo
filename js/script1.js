c78dbabdc6d02fb1decac0a2b5361dc59f5dd9b7
allysajoy.moyano.iics@ust.edu.ph <allysajoy.moyano.iics@ust.edu.ph>
2020-09-14 00:09:12 +0800

    initial project

---
+++ js/script1.js
@@ -0,0 +1,20 @@
+function login(){
+    let user = document.getElementById("user").value;
+    let pwd = document.getElementById("pwd").value;
+
+    if(user ==="admin" && pwd ==="secret"){
+        alert("you have successfully signed in");
+    }else{
+        alert("invalid credentials");
+    }
+}
+
+const temp  = 5;
+temp ="dog"; 
+
+functio m1(){
+    let temp2 = true;
+    console.log(temp);
+}
+
+console.log(temp2);
